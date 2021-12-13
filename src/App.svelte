7a986ea74e4c5c0191b8ea2f8be05e663f88db2d
HelmutvonKrieger <crissbrettd@outlook.com>
2021-12-12 18:40:14 -0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+    
+</script>
+
+<main>
+    <h1>Tree of Life Worship Music Aid</h1>
+    <div id="mainDiv">
+        hello world
+    </div>
+</main>
+
+
+
+<style>
+	main {
+        color: lightgrey;
+		text-align: center;
+		padding: 1em;
+		max-width: 500px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: darkgreen;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+    .mainButton {
+        color: darkgreen;
+        margin-top: 10px;
+    }
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
